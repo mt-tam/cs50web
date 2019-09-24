f0c890ac91749476a2463ef730df8161aef79f99
Marius Tamas <marius_tamas@outlook.com>
2019-09-24 21:16:36 +0200

    Version 2

--- application.py
+++ application.py
@@ -1,9 +1,15 @@
-import os, requests
+# --------------- IMPORT MODULES --------------- #
 
-from flask import Flask, session, render_template, request, jsonify
+import os
+import requests
+from datetime import datetime
+from flask import Flask, url_for, session, render_template, request, jsonify, redirect
 from flask_session import Session
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -21,53 +27,164 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
+# --------------- LANDING PAGE --------------- #
+
 @app.route("/")
-def index():
-    # Retrieve all books
-    books = db.execute("SELECT isbn, title, author, year FROM books LIMIT 20").fetchall()
+def landing():
+    return render_template("landing.html")
+
+
+# --------------- REGISTER --------------- #
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    # Show the Register page
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    # Send back the Register form data
+    else:
+        # Save email and password
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        # Hash password
+        hash_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+        # Check if user already exists (if yes show warning)
+        user_exists = db.execute("SELECT * FROM users WHERE email = :email", {"email":email}).fetchone()
+        
+        # Give error message back to user
+        if user_exists:
+            error_message = "Error: User is already in database. Please try again."
+            return render_template("register.html", error=error_message)
+        
+        # Create user in database, create new session and re-direct user to list
+        else:
+            db.execute("INSERT INTO users (email, password) VALUES (:email, :password)", {"email": email, "password": password})
+            session["user_id"] = user_exists["id"]
+            return redirect("/list")
+
+
+# --------------- LOGIN --------------- #
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    else:
+        # Save email and password from Login form
+        email = request.form.get("email")
+        password = request.form.get("password")
+        
+        # Check if credentials are correct
+        user_exists = db.execute("SELECT * FROM users WHERE email = :email;", {"email": email}).fetchone()
+        
+        if not user_exists:
+            # If wrong email, show warning to user
+            error_message = "Error: Email or password are not correct. Please try again."
+            return render_template("login.html", error=error_message)
+
+        else:
+            # REMOVE THIS LINE when bug on REGISTER is fixed
+            hash_password = generate_password_hash(user_exists["password"])
+
+            # Check if password is correct
+            password_correct = check_password_hash(hash_password, password)
+            
+            # If wrong password, show warning to user
+            if not password_correct:
+                error_message = "Error: Email or password are not correct. Please try again."
+                return render_template("login.html", error=error_message)
+            
+            # If correct password, create new session and redirect to list
+            else:
+                session["user_id"] = user_exists["id"]
+                return redirect("/list")
+
+
+# --------------- LOGOUT --------------- #
+
+@app.route("/logout")
+def logout():
+    session["user_id"] = None
+    return redirect ("/")
+
+
+# --------------- SHOW BOOK LIST --------------- #
+
+@app.route("/list")
+def list():
+
+    # If user is not logged in, redirect to login page
+    if session["user_id"] == None: 
+        error_message = "You must be logged in to access that page."
+        return render_template("login.html", error=error_message)
+
+    # Retrieve all books (limit = 20 books)
+    books = db.execute(
+        "SELECT isbn, title, author, year FROM books LIMIT 20").fetchall()
 
     # Render a list of all books
-    return render_template("index.html", books=books)
+    return render_template("list.html", books=books)
 
+
+# --------------- SUBMIT REVIEW --------------- #
 
 @app.route("/add_review", methods=["POST"])
 def add_review():
+
+    # If user is not logged in, redirect to login page
+    if session["user_id"] == None: 
+        error_message = "You must be logged in to access that page."
+        return render_template("login.html", error=error_message)
+    
+    # Else, save user's rating and review
     rating = request.form.get("rating")
     review = request.form.get("review")
 
     # Get book-id
     isbn = request.form.get("isbn")
-    book_id = db.execute("SELECT book_id FROM books WHERE isbn=:isbn", {"isbn":isbn})
-
-    print(f"Your rating for book #{book_id} with ISBN:{isbn} is {rating} and your review is \n {review}")
-
-    # Get user-id from session
-
+    book_id = db.execute(
+        "SELECT id FROM books WHERE isbn=:isbn", {"isbn": isbn}).fetchone()
     # Get current date and time
+    now = datetime.now()
 
+    print(
+        f"Your rating was created on {now} for book #{book_id['id']} with ISBN:{isbn} is {rating} and your review is // {review} //")
+    
     # Add rating & review to database
+    db.execute("INSERT INTO reviews (book_id, user_id, rating, review, created_on) VALUES (:book_id, :user_id, :rating, :review, :created_on)", {"book_id" : book_id["id"], "user_id" : session["user_id"], "rating" : rating, "review" : review, "created_on": now})
+    
+    # Reload page with review
+    return redirect(url_for('book') + "?isbn=" + isbn)
 
 
-    return render_template("index.html")
-
+# --------------- SEARCH BOOKS --------------- #
 
 @app.route("/search")
 def search():
-    keyword = int(request.args.get("keyword"))
-
-    """
-    # Find books in database (currently: by YEAR, should be: TITLE, IBSN, AUTHOR)
-    books = db.execute("SELECT * FROM books WHERE year= :keyword ", {"keyword":keyword}).fetchall()
-    return render_template("index.html", books=books)
-    """
-
-    # Based on title
+    # Search based on title, isbn or author
     keyword = request.args.get("keyword")
-    keyword = "%" + keyword + "%"
-    print(keyword)
-    books = db.execute("SELECT * FROM books WHERE LOWER(title) LIKE :keyword", {"keyword":keyword.lower()}).fetchall()
-    return render_template("index.html", books=books)
+    
+    # Format the search keyword (except if numeric)
+    if not keyword.isnumeric():
+        keyword = "%" + keyword + "%"
 
+    # Find similar books in database
+    books = db.execute("SELECT * FROM books WHERE LOWER(title) LIKE :keyword OR LOWER(author) LIKE :keyword OR isbn=:keyword",
+                       {"keyword": keyword.lower()}).fetchall()
+    print(books)
+    if len(books) == 0:
+        error_message="No books were found. Please try a different keyword."
+        return render_template("list.html", error=error_message)
+    else:
+        return render_template("list.html", books=books)
+
+
+# --------------- BOOK DETAILS --------------- #
 
 # Display book page
 @app.route("/book")
@@ -75,10 +192,12 @@ def book():
     isbn = request.args.get("isbn")
 
     # Find book in database
-    book = db.execute("SELECT isbn, title, author, year FROM books WHERE isbn = :isbn", {"isbn":isbn}).fetchone()
+    book = db.execute("SELECT isbn, title, author, year FROM books WHERE isbn = :isbn", {
+                      "isbn": isbn}).fetchone()
 
     # Retrieve ratings count and average rating from Goodreads API
-    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "lWS8cyNZMHofTipApJbzw", "isbns": isbn})
+    res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                       params={"key": "lWS8cyNZMHofTipApJbzw", "isbns": isbn})
 
     # Parse JSON response
     ratings_count = res.json()["books"][0]["work_ratings_count"]
@@ -98,15 +217,19 @@ def book():
     return render_template("book.html", book=book)
 
 
+# --------------- API: BOOK INFO --------------- #
+
 # API to send book info
 @app.route("/api/<string:isbn>")
 def get_book(isbn):
 
     # Find book in database
-    book = db.execute("SELECT isbn, title, author, year FROM books WHERE isbn = :isbn", {"isbn":isbn}).fetchone()
+    book = db.execute("SELECT isbn, title, author, year FROM books WHERE isbn = :isbn", {
+                      "isbn": isbn}).fetchone()
 
     # Retrieve ratings count and average rating from Goodreads API
-    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "lWS8cyNZMHofTipApJbzw", "isbns": isbn})
+    res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                       params={"key": "lWS8cyNZMHofTipApJbzw", "isbns": isbn})
 
     # Parse JSON response
     ratings_count = res.json()["books"][0]["work_ratings_count"]
